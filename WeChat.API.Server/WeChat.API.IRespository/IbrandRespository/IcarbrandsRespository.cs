c6ab02de27c422721f90daadc18716a3ce6d7ce0
ZLK99 <46181806+ZLK99@users.noreply.github.com>
2019-02-18 09:38:11 +0800

    1

---
+++ WeChat.API.Server/WeChat.API.IRespository/IbrandRespository/IcarbrandsRespository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeChat.API.Model.brand;
+
+namespace WeChat.API.IRespository.IbrandRespository
+{
+    public interface IcarbrandsRespository
+    {
+        /// <summary>
+        /// 汽车品牌的展示
+        /// </summary>
+        /// <returns></returns>
+        List<Carbrands> GetCarbrands();
+    }
+}
---
+++ WeChat.API.Server/WeChat.API.Model/brand/Brancars.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChat.API.Model.brand
+{
+    public class Brancars
+    {
+        /// <summary>
+        /// 汽车表ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 车型大全的id
+        /// </summary>
+        public int CarSid { get; set; }
+
+        /// <summary>
+        /// 汽车详情的名字
+        /// </summary>
+        public string CarName { get; set; }
+
+        /// <summary>
+        /// 汽车详情的图片
+        /// </summary>
+        public string CarImg { get; set; }
+
+        /// <summary>
+        /// 汽车的价钱
+        /// </summary>
+        public string CarPrice { get; set; }
+    }
+}
