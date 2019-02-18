64b698345d9ff435b33e77361c6cbc320ec2018c
ZLK99 <46181806+ZLK99@users.noreply.github.com>
2019-02-18 20:03:36 +0800

    1

---
+++ WeChat.API.Server/WeChat.API.Respository/BrandRespository/CarMessageRespository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeChat.API.Model.brand;
+using WeChat.API.IRespository.IbrandRespository;
+using System.Data;
+using System.Configuration;
+using Dapper;
+using Oracle.DataAccess.Client;
+
+namespace WeChat.API.Respository.BrandRespository
+{
+    public class CarMessageRespository : IcarMessageRespositpry
+    {
+        public static IDbConnection conn = new OracleConnection(ConfigurationManager.ConnectionStrings["OraclConn"].ToString());
+
+        public List<CarMessage> GetCarMessages(int id)
+        {
+            string sql = "select * from carmessage where carsid="+id;
+            var carMessagesList = conn.Query<CarMessage>(sql);
+            return carMessagesList.ToList();
+        }
+    }
+}
